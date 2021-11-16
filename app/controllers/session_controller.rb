072b2c4e9a48a61ae7785fbcba40e712d7e3ae96
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-11-16 05:15:30 +0000

    Added user authentication RSPEC testing + refactored authentication of user on login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,8 +3,9 @@ class SessionController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password]) #verifies that user exists and password is correct/authenticated
+    # @user = User.find_by_email(params[:email])
+    # if @user && @user.authenticate(params[:password]) #verifies that user exists and password is correct/authenticated
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = @user.id #save the user.id to the browser cookie. Keeps the user logged in and able to go around website
       redirect_to products_path
     else #if login does not correct and email or password is incorrect HOW TO ADD ERROR MESSAGE?
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,33 +10,33 @@ RSpec.describe Product, type: :model do
       product = Product.new(name: "Hockey Stick", quantity: 5, price: 55.55, category: @category)
       expect(product.save).to be true
     end
-    
+
     it "it fails to save if name is not present" do
       product = Product.new(name: nil, quantity: 5, price: 55.55, category: @category)
       expect(product.save).to be false
       expect(product.errors.full_messages).to include(product.errors.full_messages[0])
-      p product.errors.full_messages
+      # p product.errors.full_messages
     end
 
     it "it fails to save if price is not present" do
       product = Product.new(name: "Hockey Stick", quantity: 5, price: nil, category: @category)
       expect(product.save).to be false
       expect(product.errors.full_messages).to include(product.errors.full_messages[0])
-      p product.errors.full_messages
+      # p product.errors.full_messages
     end
 
     it "it fails to save if quantity is not present" do
       product = Product.new(name: "Tennis Racket", quantity: nil, price: 55.55, category: @category)
       expect(product.save).to be false
       expect(product.errors.full_messages).to include(product.errors.full_messages[0])
-      p product.errors.full_messages
+      # p product.errors.full_messages
     end
 
     it "it fails to save if category is not present" do
       product = Product.new(name: "BaseBall", quantity: 5, price: 55.55, category: nil)
       expect(product.save).to be false
       expect(product.errors.full_messages).to include(product.errors.full_messages[0])
-      p product.errors.full_messages
+      # p product.errors.full_messages
     end
 
   end
