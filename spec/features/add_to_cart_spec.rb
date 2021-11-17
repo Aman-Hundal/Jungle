9cba76914febab69b40e4edfe6c0993d6356acfe
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-11-17 01:18:00 +0000

    Added feature spec testing for when users add to cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to product details page from home page", type: :feature, js: true do
+RSpec.feature "Visitor Navigates to HomePage and Adds Product to Cart ", type: :feature do
   before :each do #creates 10 products for apparel category beach each test is run
     @category = Category.create! name: 'Apparel'
     10.times do |n|
@@ -14,14 +14,13 @@ RSpec.feature "Visitor navigates to product details page from home page", type: 
     end
   end
 
-  scenario "They see product details for selected product" do
+  scenario "They see MyCart increase from 0 to 1" do
     visit root_path
-    page.find(".products article:first-child").click_on "Details"
+    expect(page).to have_content("My Cart (0)")
 
+    page.find(".products article:first-child").click_on "Add"
     
-    expect(page).to have_css 'article.product-detail', count: 1
-    save_and_open_screenshot
+    expect(page).to have_content("My Cart (1)")
+    puts page.html
   end
-
-
 end
